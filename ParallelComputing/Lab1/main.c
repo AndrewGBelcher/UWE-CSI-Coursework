c962beb8df90ebd717803a1482996828b0602bf9
Andrew Belcher <andydrew1523@gmail.com>
2019-09-25 13:01:10 +0100

    more changes

--- ParallelComputing/Lab1/main.c
+++ ParallelComputing/Lab1/main.c
@@ -2,55 +2,31 @@
 #include <omp.h>
 #include <vector>
 #include <stdlib.h>
+#include <string.h>
 
 using namespace std;
 
-int segment_read(char *buff, const int len, const int count) {
-  return 1;  
+
+
+void process_buffer(char* buffer, size_t size) 
+{
+	int nums[size];
+	printf("size:%d\n");
+	int i = 0;
+	char *token;
+
+	//printf("%s",buffer);
+
+	while ((token = strtok(buffer, " ")) != NULL) 
+	{
+	    nums[i] = atoi(token);
+	    printf("num %d:%d token:%s\n",i,nums[i],buffer[i]);
+	}
+
 }
 
-void foo(char* buffer, size_t size) {
-    int count_of_reads = 0;
-    int count = 1;
-    std::vector<int> *posa;
-    int nthreads;
-
-    #pragma omp parallel 
-    {
-        nthreads = omp_get_num_threads();
-        const int ithread = omp_get_thread_num();
-        #pragma omp single 
-        {
-            posa = new vector<int>[nthreads];
-            posa[0].push_back(0);
-        }
-
-        //get the number of lines and end of line position
-        #pragma omp for reduction(+: count)
-        for(int i=0; i<size; i++) {
-            if(buffer[i] == '\n') { //should add EOF as well to be safe
-                count++;
-                posa[ithread].push_back(i);
-            }
-        }
-
-        #pragma omp for     
-        for(int i=1; i<count ;i++) {    
-            const int len = posa[ithread][i] - posa[ithread][i-1];
-            char* buff = &buffer[posa[ithread][i-1]];
-            const int sequence_counter = segment_read(buff,len,i);
-            if (sequence_counter == 1) {
-                #pragma omp atomic
-                count_of_reads++;
-                printf("\n Total No. of reads: %d \n",count_of_reads);
-            }
-
-        }
-    }
-    delete[] posa;
-}
-
-int main () {
+int main () 
+{
   FILE * f;
   long lSize;
   char * buffer;
@@ -73,7 +49,7 @@ int main () {
   if (result != lSize) {fputs ("Reading error",stderr); exit (3);}
 
   /* the whole file is now loaded in the memory buffer. */
-  foo(buffer, result);
+  process_buffer(buffer, result);
   // terminate
 
 
